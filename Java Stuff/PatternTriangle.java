6f663fcaafbcaea57ba550e9234dae8d8e81f119
Vasanth N <vasanthnimmala69@outlook.com>
2022-12-08 20:51:41 +0530

    Create PatternTriangle.java

---
+++ Java Stuff/PatternTriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args){
+		int n, num = 1 ;
+		String blank = " " ;
+		System.out.println( "Enter no. of rows:" );
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		for ( int i = 1 ; i < 2 * n; i += 2 ){
+			System.out.println(blank.repeat( 2 * n - i - 1 ) + (Integer.toString(num)).repeat(i));
+			num+=1;
+		}
+	}
+}
